affa648370985562e397969890756531085dc9f6
hsiaofongw <hsiaofong.w@gmail.com>
2021-03-15 13:19:04 +0800

    update

--- pages/api/cron.ts
+++ pages/api/cron.ts
@@ -2,10 +2,31 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 async function requestHandler(req: NextApiRequest, res: NextApiResponse) {
 
-    const headers = req.headers;
-    console.log(headers);
+    let logObject = { 
+        headers: {
+            host: '',
+            authorization: '',
+            'x-matched-path': '',
+            'x-forwarded-host': '',
+            accept: '',
+            'x-vercel-deployment-url': '',
+            'x-forwarded-proto': '',
+            'x-forwarded-for': '',
+            'user-agent': '',
+            'x-vercel-forwarded-for': '',
+            'x-real-ip': '',
+            'x-vercel-id': '',
+            connection: '',
+            'transfer-encoding': ''
+        }
+    };
 
-    res.status(200).json({ text: 'Hello' });
+    for (const k in req.headers) {
+        logObject.headers[k] = req.headers[k];
+    }
+
+    console.log(logObject);
+    res.status(200).json(logObject);
 }
 
 
