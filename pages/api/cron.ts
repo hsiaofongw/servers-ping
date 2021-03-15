0a1526c8175f1b3337568c4c3aa0a80c1dc12f7e
hsiaofongw <hsiaofong.w@gmail.com>
2021-03-15 13:11:00 +0800

    update

--- pages/api/cron.ts
+++ pages/api/cron.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from 'next';
 
 async function requestHandler(req: NextApiRequest, res: NextApiResponse) {
 
--- pages/api/hello.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-
-async function requestHandler(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json({ text: 'Hello' })
-}
-
-export default requestHandler;
