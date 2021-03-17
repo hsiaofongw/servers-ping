cd23312f1363ae3b6648f584fbc0cea054317b50
hsiaofongw <hsiaofong.w@gmail.com>
2021-03-18 06:18:12 +0800

    update

--- pages/api/watchlist.ts
+++ pages/api/watchlist.ts
@@ -3,6 +3,7 @@ import data from '../../data/watchList.json';
 
 async function requestHandler(req: NextApiRequest, res: NextApiResponse<IWatchTarget[]>) {
 
+    res.setHeader('Cache-Control', 'public, max-age=600');
     res.status(200).json(data);
     
 }
